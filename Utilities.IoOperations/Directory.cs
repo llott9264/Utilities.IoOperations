7cb739ab450bb5f096412027e3594a085e6eea6a
Lee Lott <llott88@gmail.com>
2024-06-01 17:57:19 -0500

    Initial code check in. resolves #1.

---
+++ Utilities.IoOperations/Directory.cs
@@ -0,0 +1,32 @@
+﻿namespace Utilities.IoOperations;
+
+public class Directory
+{
+	public static void DeleteDirectory(DirectoryInfo directory, int retentionLengthInMonths, bool isBaseFolder)
+	{
+		directory.EnumerateFiles()
+			.Where(f => f.LastAccessTime < DateTime.Now.AddMonths(retentionLengthInMonths))
+			.ToList()
+			.ForEach(f => { f.Delete(); });
+
+		directory.EnumerateDirectories()
+			.Where(d => d.LastAccessTime < DateTime.Now.AddMonths(retentionLengthInMonths))
+			.ToList()
+			.ForEach(d => { DeleteDirectory(d, retentionLengthInMonths, false); });
+
+		if (!isBaseFolder
+		    && directory.EnumerateFiles().ToList().Count == 0
+		    && directory.EnumerateDirectories().ToList().Count == 0)
+		{
+			directory.Delete();
+		}
+	}
+	
+	public static void CreateDirectory(string folder)
+	{
+		if (!System.IO.Directory.Exists(folder))
+		{
+			System.IO.Directory.CreateDirectory(folder);
+		}
+	}
+}
---
+++ Utilities.IoOperations/File.cs
@@ -0,0 +1,21 @@
+﻿namespace Utilities.IoOperations
+{
+	public class File
+	{
+		public static bool Move(string sourceFile, string destinationFolder)
+		{
+			bool isFileMoved = false;
+			string destinationFile = $@"{destinationFolder}\{Path.GetFileName(sourceFile)}";
+
+			Directory.CreateDirectory(destinationFolder);
+
+			if (!System.IO.File.Exists(destinationFolder))
+			{
+				System.IO.File.Move(sourceFile, destinationFile);
+				isFileMoved = true;
+			}
+
+			return isFileMoved;
+		}
+	}
+}
