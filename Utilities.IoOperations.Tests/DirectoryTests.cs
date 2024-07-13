17bb24333b5c29175af8a811582c7c5c2da6ceff
Lee Lott <llott88@gmail.com>
2024-07-13 14:33:15 -0500

    #2 - Completed unit tests for IO Operations.

--- Utilities.IoOperations.Tests/DirectoryTests.cs
+++ Utilities.IoOperations.Tests/DirectoryTests.cs
@@ -15,5 +15,43 @@ namespace Utilities.IoOperations.Tests
 			//Assert
 			Assert.True(System.IO.Directory.Exists(directoryPath));
 		}
+
+		[Fact]
+		public void DeleteDirectory_Returns_True()
+		{
+			//Arrange
+			const string deleteDirectoryPath = @"DirectoryFolder\DeleteDirectory\";
+			DirectoryInfo di = new(deleteDirectoryPath);
+			int fileRetentionPeriodInMonths = -1;
+			const string deletedFolderPath = $@"{deleteDirectoryPath}\DirectoryToDelete\";
+			const string deletedFolderFileToDelete = $@"{deleteDirectoryPath}\DirectoryToDelete\FileToDelete.txt";
+
+			const string notDeletedFolderPath = $@"{deleteDirectoryPath}\DirectoryNotToDelete\";
+			const string notDeletedFolderFileToDelete = $@"{notDeletedFolderPath}\FileToDelete.txt";
+			const string notDeletedFile = $@"{notDeletedFolderPath}\FileNotToDelete.txt";
+
+			DateTime deleteLastAccessDateTime = DateTime.Now.AddMonths(-3);
+			DateTime notDeleteLastAccessDateTime = DateTime.Now.AddDays(-3);
+
+			System.IO.Directory.SetLastAccessTime(deletedFolderPath, deleteLastAccessDateTime);
+			System.IO.File.SetLastAccessTime(deletedFolderFileToDelete, deleteLastAccessDateTime);
+
+			System.IO.Directory.SetLastAccessTime(notDeletedFolderPath, notDeleteLastAccessDateTime);
+			System.IO.File.SetLastAccessTime(notDeletedFolderFileToDelete, deleteLastAccessDateTime);
+			System.IO.File.SetLastAccessTime(notDeletedFile, notDeleteLastAccessDateTime);
+			
+			//Act
+			Directory.DeleteDirectory(di, fileRetentionPeriodInMonths, true);
+
+			//Assert
+			Assert.True(!System.IO.Directory.Exists(deletedFolderPath));
+			Assert.True(System.IO.Directory.Exists(notDeletedFolderPath));
+
+			Assert.True(!System.IO.File.Exists(deletedFolderFileToDelete));
+			Assert.True(!System.IO.File.Exists(notDeletedFolderFileToDelete));
+			Assert.True(System.IO.File.Exists(notDeletedFile));
+
+			Assert.True(System.IO.Directory.Exists(deleteDirectoryPath));
+		}
 	}
 }
