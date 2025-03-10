6faac0966c95e4571fe6cdd9aec6bd8e660f6cbc
Lee Lott <llott88@gmail.com>
2025-03-10 18:47:43 -0500

    #15 - Added code to register MediatR.

---
+++ Utilities.IoOperations/IoOperationsServiceRegistration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Utilities.IoOperations;
+
+public static class IoOperationsServiceRegistration
+{
+	public static IServiceCollection AddIoOperationsServices(this IServiceCollection services)
+	{
+		services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+		return services;
+	}
+}
